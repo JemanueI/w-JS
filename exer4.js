895fb1ca74467f97ab1cab6b72837fe7f3687f7f
Jemanuel <jtemanueljt@gmail.com>
2023-01-03 23:44:37 -0300

    novo exer

---
+++ exer4.js
@@ -0,0 +1,23 @@
+class Carros {
+    marca;
+    cor;
+    mediaGastoPorKm;
+
+    constructor (marca, cor, mediaGastoPorKm) {
+        this.marca = marca;
+        this.cor = cor;
+        this.mediaGastoPorKm = mediaGastoPorKm;
+    }
+
+    calcularGastoPercurso(distancia, precoComb){
+        return (distancia * this.mediaGastoPorKm) * precoComb;
+    }
+}
+
+const uno = new Carros('Fiat','Branco', 1/12);
+uno.calcularGastoPercurso(70, 5);
+
+console.log(uno.calcularGastoPercurso(70, 5);)
+
+
+
